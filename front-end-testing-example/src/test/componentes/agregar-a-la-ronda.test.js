228e0e81331b2402e62860a6e99cfdd2f42b5394
ltrinidad <lucasleonardo1996@hotmail.com>
2019-04-15 11:08:54 -0300

    test AgregarALaRonda2 001 - 'Cuando se ingresa un nombre valido se agregar una persona a la ronda'

--- front-end-testing-example/src/test/componentes/agregar-a-la-ronda.test.js
+++ front-end-testing-example/src/test/componentes/agregar-a-la-ronda.test.js
@@ -1,46 +1,28 @@
 import React from 'react';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { shallow } from 'enzyme';
-import {AgregarALaRonda} from "../../Componentes/AgregarALaRonda";
 import {RondaDeMates} from "../../RondaDeMates";
 
 configure({ adapter: new Adapter() });
 
 let rondaDeMates = new RondaDeMates();
 
-it('Se renderiza un boton', () => {
-    const componente = shallow(<AgregarALaRonda/>);
+const agregarPersonaA = (unaRondaDeMates) => (unaPersona) => {
+    if(unaPersona !== "") {
+        unaRondaDeMates.agregar(unaPersona)
+    }
+};
 
-    expect(componente.find('.boton-agregar')).toHaveLength(1);
+afterEach(() => {
+    rondaDeMates = new RondaDeMates()
 });
 
-describe('Cuando se oprime el boton', () => {
-    beforeEach(() => {
-        rondaDeMates = new RondaDeMates();
-    });
+describe('Cuando se ingresa un nombre valido', () => {
 
-    const agregarPersonaA = (unaRondaDeMates) => (unaPersona) => {unaRondaDeMates.agregar(unaPersona)};
+    const unNombreValido = 'lalo';
 
-    describe('y se ingreso un nombre valido', () => {
-        const unNombreValido = 'lalo';
-
-        it('se agrega una persona a la ronda', () => {
-            const componente = shallow(<AgregarALaRonda personaAAgregar={unNombreValido} agregarPersona={agregarPersonaA(rondaDeMates)} />);
-
-            componente.find('.boton-agregar').simulate('click');
-            expect(rondaDeMates.proximo()).toEqual(unNombreValido);
-        })
-    });
-
-    describe('y se ingreso un nombre invalido', () => {
-        const unNombreInvalido = '';
-
-        it('no se agrega una persona a la ronda', () => {
-            const componente = shallow(<AgregarALaRonda personaAAgregar={unNombreInvalido} agregarPersona={agregarPersonaA(rondaDeMates)} />);
-
-            componente.find('.boton-agregar').simulate('click');
-            expect(rondaDeMates.proximo()).toEqual(RondaDeMates.noHayNadie);
-        })
-    });
+    it('se agrega una persona a la ronda', () => {
+        agregarPersonaA(rondaDeMates)(unNombreValido);
+        expect(rondaDeMates.proximo()).toEqual(unNombreValido);
+    })
 });
