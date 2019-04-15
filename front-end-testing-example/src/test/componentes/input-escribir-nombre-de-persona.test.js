25c7e51fb70a5ed86ec78ca873167decbf21fa80
ltrinidad <lucasleonardo1996@hotmail.com>
2019-04-15 15:02:16 -0300

    test EscribirNombreDePersona2 004 - 'Cuando se escribe una letra en el input se llama al callback'

---
+++ front-end-testing-example/src/test/componentes/input-escribir-nombre-de-persona.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {EscribirNombreDePersona} from "../../Componentes/EscribirNombreDePersona";
+
+configure({ adapter: new Adapter() });
+
+const callback = jest.fn(() => null);
+
+describe('Cuando se escribe una letra', () => {
+    it('se llama al callback', () => {
+        const componente = shallow(<EscribirNombreDePersona actualizar={callback}/>);
+
+        componente.find('.input-agregar').simulate('change', {}, {value: 'f'});
+
+        expect(callback).toBeCalled();
+    })
+});
