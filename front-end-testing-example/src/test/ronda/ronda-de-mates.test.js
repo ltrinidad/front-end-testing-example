c8802567d93c686e85bc2c87d7b109592a00c474
ltrinidad <lucasleonardo1996@hotmail.com>
2019-04-22 15:57:58 -0300

    Agrego un beforeEach en los tests de ronda de mates

--- front-end-testing-example/src/test/ronda/ronda-de-mates.test.js
+++ front-end-testing-example/src/test/ronda/ronda-de-mates.test.js
@@ -6,23 +6,26 @@ function rondaDeMates() {
     return new RondaDeMates(new RondaVacia());
 }
 
+let ronda;
+
+beforeEach(() => {
+	ronda = rondaDeMates();
+})
+
 const angie = 'angie'
 const feche = 'feche'
 const lalo = 'lalo'
 
 test('Cuando no hay nadie en la ronda y se pide el proximo se obtiene que no hay nadie en la ronda', () => {
-    const ronda = rondaDeMates();
     expect(ronda.proximo()).toEqual(RondaDeMates.noHayNadie)
 });
 
 test('Cuando se agrega una persona a la ronda y se pide el proximo se obtiene el nombre de esa persona', () => {
-    const ronda = rondaDeMates();
     const nuevaRonda = ronda.agregar(feche);
     expect(nuevaRonda.proximo()).toEqual(feche);
 });
 
 test('Cuando se agregan dos personas y se pide el proximo se obtiene la primera que ingreso, luego si se pide de nuevo el proximo se obtiene la segunda', () => {
-    const ronda = rondaDeMates();
     const nuevaRonda = ronda.agregar(feche)
                             .agregar(angie);
     expect(nuevaRonda.proximo()).toEqual(feche);
@@ -30,7 +33,6 @@ test('Cuando se agregan dos personas y se pide el proximo se obtiene la primera 
 });
 
 test('Cuando se agregan tres personas y se pide el proximo 4 veces se obtiene el primero que ingreso, luego si se pide de nuevo el proximo se obtiene el segundo', () => {
-    const ronda = rondaDeMates();
     const nuevaRonda = ronda.agregar(feche)
                             .agregar(angie)
                             .agregar(lalo);
