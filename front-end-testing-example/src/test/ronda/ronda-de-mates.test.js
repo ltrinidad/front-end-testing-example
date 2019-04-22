95c266b60a3026ec7bee46ac724d6c61871da178
ltrinidad <lucasleonardo1996@hotmail.com>
2019-04-22 15:50:36 -0300

    Refactor en los tests de ronda de mates, convierto los strings sueltos en constantes

--- front-end-testing-example/src/test/ronda/ronda-de-mates.test.js
+++ front-end-testing-example/src/test/ronda/ronda-de-mates.test.js
@@ -6,6 +6,10 @@ function rondaDeMates() {
     return new RondaDeMates(new RondaVacia());
 }
 
+const angie = 'angie'
+const feche = 'feche'
+const lalo = 'lalo'
+
 test('Cuando no hay nadie en la ronda y se pide el proximo se obtiene que no hay nadie en la ronda', () => {
     const ronda = rondaDeMates();
     expect(ronda.proximo()).toEqual(RondaDeMates.noHayNadie)
@@ -13,35 +17,35 @@ test('Cuando no hay nadie en la ronda y se pide el proximo se obtiene que no hay
 
 test('Cuando se agrega una persona a la ronda y se pide el proximo se obtiene el nombre de esa persona', () => {
     const ronda = rondaDeMates();
-    const nuevaRonda = ronda.agregar('feche');
-    expect(nuevaRonda.proximo()).toEqual('feche');
+    const nuevaRonda = ronda.agregar(feche);
+    expect(nuevaRonda.proximo()).toEqual(feche);
 });
 
 test('Cuando se agregan dos personas y se pide el proximo se obtiene la primera que ingreso, luego si se pide de nuevo el proximo se obtiene la segunda', () => {
     const ronda = rondaDeMates();
-    const nuevaRonda = ronda.agregar('feche')
-                            .agregar('angie');
-    expect(nuevaRonda.proximo()).toEqual('feche');
-    expect(nuevaRonda.proximo()).toEqual('angie')
+    const nuevaRonda = ronda.agregar(feche)
+                            .agregar(angie);
+    expect(nuevaRonda.proximo()).toEqual(feche);
+    expect(nuevaRonda.proximo()).toEqual(angie)
 });
 
 test('Cuando se agregan tres personas y se pide el proximo 4 veces se obtiene el primero que ingreso, luego si se pide de nuevo el proximo se obtiene el segundo', () => {
     const ronda = rondaDeMates();
-    const nuevaRonda = ronda.agregar('feche')
-                            .agregar('angie')
-                            .agregar('lalo');
+    const nuevaRonda = ronda.agregar(feche)
+                            .agregar(angie)
+                            .agregar(lalo);
 
     [1,2,3].forEach(() => nuevaRonda.proximo());
 
-    expect(nuevaRonda.proximo()).toEqual('feche');
-    expect(nuevaRonda.proximo()).toEqual('angie')
+    expect(nuevaRonda.proximo()).toEqual(feche);
+    expect(nuevaRonda.proximo()).toEqual(angie)
 });
 
 test('Cuando ya hay dos personas y se pide el proximo se obtiene la primera que ingreso, luego si se pide de nuevo el proximo se obtiene la segunda', () => {
-    const ronda = new RondaDeMates(new RondaNoVacia(['feche', 'angie', 'lalo']));
+    const ronda = new RondaDeMates(new RondaNoVacia([feche, angie, lalo]));
 
     [1,2,3].forEach(() => ronda.proximo());
 
-    expect(ronda.proximo()).toEqual('feche');
-    expect(ronda.proximo()).toEqual('angie')
+    expect(ronda.proximo()).toEqual(feche);
+    expect(ronda.proximo()).toEqual(angie)
 });
