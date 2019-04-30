ca8733d09b777cc3465313383333c857866592c2
ltrinidad <lucasleonardo1996@hotmail.com>
2019-04-30 12:18:56 -0300

    Refactor en la estructura de los tests de escribir nombre

--- front-end-testing-example/src/test/componentes/escribir-nombre-de-persona.test.js
+++ front-end-testing-example/src/test/componentes/escribir-nombre-de-persona.test.js
@@ -8,63 +8,71 @@ import {sinUltimaLetra} from "../../Funciones/letras";
 
 configure({ adapter: new Adapter() });
 
-let personaAAgregar = '';
-let componente;
-
-beforeEach(() => {
-    personaAAgregar = '';
-    componente = shallow(<EscribirNombreDePersona agregarLetra={agregarLetraAlNombre} borrarUltimaLetra={borrarUltimaLetra}/>);
-});
-
 const agregarLetraAlNombre = (nuevaLetra) => {
+
     personaAAgregar = nombreAPartirDe(personaAAgregar, nuevaLetra)
 };
-
 const borrarUltimaLetra = () => {
+
     personaAAgregar = sinUltimaLetra(personaAAgregar)
 };
 
-it('Se renderiza un input', () => {
-    expect(componente.find('.input-agregar')).toHaveLength(1);
+let personaAAgregar = '';
+let componenteEscribirNombre = <EscribirNombreDePersona agregarLetra={agregarLetraAlNombre} borrarUltimaLetra={borrarUltimaLetra}/>;
+
+beforeEach(() => {
+    personaAAgregar = '';
+    componente = shallow(componenteEscribirNombre)
 });
 
-describe('Cuando no se escribe una letra', () => {
-    const vacio = '';
+let componente = shallow(componenteEscribirNombre);
 
-    it('no se agrega nada al nombre de la persona', () => {
-        componente.find('.input-agregar').simulate('keydown', {key: vacio});
+describe('Dado un input', () => {
+    let input = componente.find('.input-agregar');
 
-        expect(personaAAgregar).toEqual(vacio);
-    })
-});
-
-describe('Cuando se escribe una letra', () => {
-    const primeraLetraDelNombre = 'f';
-
-    it('se agrega al nombre de la persona', () => {
-        componente.find('.input-agregar').simulate('keyDown', {key: primeraLetraDelNombre});
-
-        expect(personaAAgregar).toEqual(primeraLetraDelNombre);
+    it('es renderizado', () => {
+        expect(input).toHaveLength(1);
     });
 
-    describe('y luego se oprime la tecla para borrar', () => {
-        const teclaDeBorrar = 'Backspace';
+    describe('cuando no se escribe una letra', () => {
+        const vacio = '';
 
-        it('el nombre pierde la ultima letra', () => {
-            componente.find('.input-agregar').simulate('keydown', {key: primeraLetraDelNombre});
-            componente.find('.input-agregar').simulate('keyDown', {key: teclaDeBorrar});
+        it('no se agrega nada al nombre de la persona', () => {
+            input.simulate('keydown', {key: vacio});
 
-            expect(personaAAgregar).toEqual('');
+            expect(personaAAgregar).toEqual(vacio);
+        })
+    });
+
+    describe('cuando se escribe una letra', () => {
+        const primeraLetraDelNombre = 'f';
+
+        it('se agrega al nombre de la persona', () => {
+            input.simulate('keyDown', {key: primeraLetraDelNombre});
+
+            expect(personaAAgregar).toEqual(primeraLetraDelNombre);
+        });
+
+        describe('y luego se oprime la tecla para borrar', () => {
+            const teclaDeBorrar = 'Backspace';
+
+            it('el nombre pierde la ultima letra', () => {
+                input.simulate('keydown', {key: primeraLetraDelNombre});
+                input.simulate('keyDown', {key: teclaDeBorrar});
+
+                expect(personaAAgregar).toEqual('');
+            })
+        });
+    });
+
+    describe('cuando se escribe un espacio', () => {
+        const espacio = ' ';
+
+        it('no cambia el nombre de la persona', () => {
+            input.simulate('keydown', {key: espacio});
+
+            expect(personaAAgregar).toEqual("");
         })
     });
 });
 
-describe('Cuando se escribe un espacio', () => {
-    const espacio = ' ';
-
-    it('no cambia el nombre de la persona', () => {
-        componente.find('.input-agregar').simulate('keydown', {key: espacio});
-
-        expect(personaAAgregar).toEqual("");
-    })
-});
